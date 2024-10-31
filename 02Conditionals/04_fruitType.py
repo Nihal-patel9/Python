9d1f289ac45356d0a32d7760a1863c167891f4ce
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-10-31 12:28:56 +0530

    Add files via upload

---
+++ 02Conditionals/04_fruitType.py
@@ -0,0 +1,11 @@
+fruit = input("Enter your fruit = ")
+
+color = input("Enter Fruit color =")
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
