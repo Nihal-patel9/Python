2b8329c07e789319c41724361158cdd176135be7
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-11-03 12:05:42 +0530

    Add files via upload

---
+++ 02Conditionals/10_match.py
@@ -0,0 +1,14 @@
+num = int(input("Enter any Number = "))
+
+match num:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case 4:
+        print("Four")
+
+
+print("Outside Match")
