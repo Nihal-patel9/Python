ba07f3e7b637c5edc259e2747f156fdfbb6a7e36
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-11-14 13:40:47 +0530

    Add files via upload

---
+++ 03Loops/06_sumEven.py
@@ -0,0 +1,10 @@
+# count positive sum number
+
+n = int(input("Enter any Number = "))
+sum_even = 0
+
+for i in range(1, n+1):
+    if i % 2 == 0:
+        sum_even += 1
+
+print("Sum of even number is: , ", sum_even)
