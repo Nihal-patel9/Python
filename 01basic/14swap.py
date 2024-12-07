6c2dd09aae574eeecc1a0a30b40ad5138e22d58b
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-12-07 18:22:14 +0530

    Add files via upload

---
+++ 01basic/14swap.py
@@ -0,0 +1,11 @@
+
+num1 = int(input("Enter Any Number : "))
+num2 = int(input("Enter Any Number : "))
+
+print("Num1 : ", num1, "Num2 : ", num2)
+
+num1, num2 = num2, num1
+
+print("Swap")
+
+print("Num1 : ", num1, "Num2 : ", num2)
