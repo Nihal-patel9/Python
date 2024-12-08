edffa3fc9d34db3666c4590d5dbe1658a10f3c70
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-12-08 19:17:18 +0530

    Add files via upload

---
+++ 01basic/17inputList.py
@@ -0,0 +1,13 @@
+
+# input list
+
+print(" How many number you want to enter : ")
+n = int(input())
+
+l1 = []
+i = 0
+
+while i < n:
+    l1.append(int(input("Enter a number : ")))
+    i += 1
+print(l1)
