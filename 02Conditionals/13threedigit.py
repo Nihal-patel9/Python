18e2c7f7b64e2b28168af9cf8ee3191288e52397
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2024-12-09 17:46:55 +0530

    Add files via upload

---
+++ 02Conditionals/13threedigit.py
@@ -0,0 +1,9 @@
+
+# check number three digit
+
+num = int(input("Enter Three digit Number : "))
+
+if 999 >= num >= 100:
+    print("Three digit number")
+else:
+    print("Not a three digit number")
